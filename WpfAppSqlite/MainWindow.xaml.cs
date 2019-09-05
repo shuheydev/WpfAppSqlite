02fb82f90cef46d1e8ecd76dc2ff430f333359b4
shuhey <nishizawa@timedia.co.jp>
2019-09-05 20:14:35 +0900

    Add project files.

---
+++ WpfAppSqlite/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppSqlite.Models;
+
+namespace WpfAppSqlite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new MyDbContext())
+            {
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+
+                var book = new Book
+                {
+                    Title = "Hello"
+                };
+
+                context.Add(book);
+                context.SaveChanges();
+
+                label.Content = context.Books.Count();
+            }
+        }
+    }
+}
---
+++ WpfAppSqlite/Models/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfAppSqlite.Models;
+
+namespace WpfAppSqlite
+{
+    class MyDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=local.db");
+        }
+    }
+}
